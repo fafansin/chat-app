e62a631b1dc2bceb0f34233bc95f70d8797aa6cc
laviel <fafansin@gmail.com>
2024-07-29 10:36:05 +0300

    feat: added controllers

---
+++ backend/controllers/auth.controller.js
@@ -0,0 +1,11 @@
+module.exports = {
+  signup: (req, res) => {
+    console.log('signinUser')
+  },
+  login: (req, res) => {
+    console.log('loginuser')
+  },
+  logout: (req, res) => {
+    console.log('logout')
+  }
+}
--- backend/routes/auth.routes.js
+++ backend/routes/auth.routes.js
@@ -1,16 +1,10 @@
 const express = require('express');
-
+const { signup, login, logout } = require('../controllers/auth.controller.js');
 const router = express.Router();
 
-router.get('/signup', (req, res) => {
-  res.send('Signup Route')
-})
-router.get('/login', (req, res) => {
-  res.send('Login Route')
-})
-router.get('/logout', (req, res) => {
-  res.send('Logout Route')
-})
+router.get('/signup', signup)
+router.get('/login', login)
+router.get('/logout', logout)
 
 
 module.exports = router;
