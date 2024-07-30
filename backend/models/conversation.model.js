f93257c03a957ee250d2bb70a51c7985a6f425e0
laviel <fafansin@gmail.com>
2024-07-30 12:58:50 +0300

    fix: error on conversation model

--- backend/models/conversation.model.js
+++ backend/models/conversation.model.js
@@ -1,17 +1,18 @@
 const mongoose = require('mongoose');
 
-module.exports = new mongoose.Model('Conversation', new mongoose.Schema({
+module.exports = new mongoose.model('Conversation', new mongoose.Schema({
   participants:[
     {
       type:mongoose.Schema.Types.ObjectId,
       ref:"User"
-    },
-    messages:[
-      {
-        type:mongoose.Schema.Types.ObjectId,
-        ref:"Message"
-        default:[]
-      }
-    ]
+    }
+  ],
+  messages:[
+    {
+      type:mongoose.Schema.Types.ObjectId,
+      ref:"Message",
+      default:[]
+    }
   ]
+  
 }, {timestamps:true}))
