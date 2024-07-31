e7287ce1fd00c4e0f82f967b39f5b5bf4cc53e76
laviel <fafansin@gmail.com>
2024-07-31 12:44:11 +0300

    feat: retrieve user list not including logged in use

--- backend/routes/user.routes.js
+++ backend/routes/user.routes.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const router = express.Router();
-const { getUsers } = require('../controllers/user.controller.js');
+const { getUsersForSideBar } = require('../controllers/user.controller.js');
+const protectRoute = require('../middleware/protectRoute.js');
 
-router.get('/', getUsers);
+router.get('/', protectRoute, getUsersForSideBar);
 
 module.exports = router;
