b2b454d71e2680b4ebbdcc873390f6334122ca4a
laviel <fafansin@gmail.com>
2024-07-31 11:34:14 +0300

    feat: implement getting messages route

--- backend/controllers/message.controller.js
+++ backend/controllers/message.controller.js
@@ -2,6 +2,25 @@ const Message = require('../models/message.model.js');
 const Conversation = require('../models/conversation.model.js');
 
 module.exports = {
+  getMessage: async (req, res) => {
+    try {
+      const { id:userToChatId } = req.params;
+      const senderId = req.user._id;
+
+      const conversation = await Conversation.findOne({
+        participants: {$all: [senderId, userToChatId]}
+      }).populate("messages");
+
+      if(!conversation) return res.status(200).json([]);
+      
+      res.status(200).json(conversation.messages);
+
+
+    } catch (e) {
+      console.log('Error in getMessage controller:', e.message);
+      res.status(500).json({error:'Internal Server Error'});
+    }
+  },
   sendMessage: async (req, res) => {
     try{
       const { message } = req.body;
--- backend/routes/message.routes.js
+++ backend/routes/message.routes.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const router = express.Router();
-const { sendMessage } = require('../controllers/message.controller.js')
+const { sendMessage, getMessage } = require('../controllers/message.controller.js')
 const protectRoute = require('../middleware/protectRoute.js');
 
+router.get('/:id', protectRoute, getMessage);
 router.post('/send/:id', protectRoute, sendMessage);
 
 module.exports = router;
