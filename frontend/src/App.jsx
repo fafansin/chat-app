6c5c49308803b50075cfc978146d5cabf7cb3e19
laviel <fafansin@gmail.com>
2024-08-06 12:30:54 +0300

    feat: design login page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,10 @@
-import { useState } from 'react'
 import './App.css'
 import Login from '../pages/login/Login';
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <div className="p-4 h-screen flex items-center justify-center">
-      <Login></Login>
+      <Login/>
     </div>
   )
 }
